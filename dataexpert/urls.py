99d20473258ee5fa9af2cf6c417f31f8246a5e69
dirmi513 <dirmihaev@gmail.com>
2020-09-16 14:52:59 -0400

    Removed Blog App & Updated README

--- dataexpert/urls.py
+++ dataexpert/urls.py
@@ -20,6 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
     path('app/courses/', include('courses.urls')),
-    path('blog/', include('blog.urls')),
     path('', include('frontend.urls')),
 ]
