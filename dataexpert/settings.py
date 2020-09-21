14ee5797f8cde1f322ea6557e33d029ef44b5453
Daniel Irmihaev <47720356+dirmi513@users.noreply.github.com>
2020-09-21 19:41:05 -0400

    Update settings.py (#40)

--- dataexpert/settings.py
+++ dataexpert/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -105,6 +106,8 @@ DATABASES = {
         'PASSWORD': os.environ['DATAEXPERT_RDS_PASSWORD']
     }
 }
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
