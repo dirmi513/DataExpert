331e2cd4fe31201a54d629b73c764c4136cae7bc
dirmi513 <dirmihaev@gmail.com>
2020-09-21 18:47:18 -0400

    Heroku Updates CONTINUED!...

--- dataexpert/settings.py
+++ dataexpert/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
-from datetime import timedelta
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -145,3 +145,7 @@ EMAIL_HOST_PASSWORD = os.environ['DATAEXPERT_GMAIL_PWD']
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
