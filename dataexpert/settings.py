b9a303a43056e766b001aaa1609ad9fe8d01007b
Daniel Irmihaev <47720356+dirmi513@users.noreply.github.com>
2020-09-21 19:19:52 -0400

    Update settings.py

--- dataexpert/settings.py
+++ dataexpert/settings.py
@@ -146,6 +146,21 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 
 AUTH_USER_MODEL = 'authentication.CustomUser'
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+            'level': os.getenv('DJANGO_LOG_LEVEL', 'ERROR'),
+        },
+    },
+}
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
